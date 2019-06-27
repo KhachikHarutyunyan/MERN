c27f6847a5680641d7f185ce68fc55cda68853bc
KhachikHarutyunyan <khachik789@gmail.com>
2019-06-27 17:43:01 +0400

    post api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const auth = require('../../middleware/auth');
 
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
-// route GET api/posts
-// dest Test route
-// access Public
-router.get('/', (req, res) => {
-    res.send('Posts route');
+// route POST api/posts
+// create a post
+// access Private
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const user = await User.findOne({user: user.id});
 });
 
 module.exports = router;
